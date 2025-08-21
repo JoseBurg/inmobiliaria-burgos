e1085ebba5b147111ffd7faa41d58d6031ce729e
José Antonio Burgos Francisco <joseaburgos01@outlook.com>
2025-08-20 22:20:35 -0400

    ajuste

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: "/inmobiliaria-burgos",
+    base: "/inmobiliaria-burgos/",
     plugins: [react()],
 });
